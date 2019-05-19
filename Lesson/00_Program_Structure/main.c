0837f23a49530a79bf48915dadf4407be1a6aede
jm.kagaya@gmail.com <jm.kagaya@gmail.com>
2019-05-19 13:56:11 +0900

    done
    done

---
+++ Lesson/00_Program_Structure/main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/*
+
+Preprocessor Commands #include <stdio.h>
+
+Functions int main() printf(...)
+
+Variables int year;
+
+Statements & Expressions
+
+Comments
+
+
+*/
+int main()
+{
+
+    /* variable definition: */
+   int year;
+   year = 2019;
+
+
+   printf("Hello World %d \n", year); // print function
+
+    return 0; // The next line return 0; terminates the main() function and returns the value 0.
+}
