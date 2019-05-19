3d7a482aaef085eea179b4337b7a9045ee979c56
jm.kagaya@gmail.com <jm.kagaya@gmail.com>
2019-05-19 20:05:47 +0900

    done
    done

---
+++ Lesson/02_Varaibles/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+/*
+A variable is nothing but a name given to a storage area
+that our programs can manipulate. Each variable in C has
+a specific type, which determines the size and layout of
+the variable's memory; the range of values that can be
+stored within that memory; and the set of operations that
+ can be applied to the variable.
+
+The name of a variable can be composed of letters, digits,
+and the underscore character. It must begin with either a
+letter or an underscore. Upper and lowercase letters are
+distinct because C is case-sensitive. Based on the basic
+types explained in the previous chapter, there will be
+the following basic variable types
+
+
+*/
+int main()
+{
+
+
+
+
+
+    return 0;
+}
